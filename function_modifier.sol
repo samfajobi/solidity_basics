3dfaf7d2dffd7b9e44359ae9c7ea2726d059f183
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-07-12 01:15:32 +0100

    function_modifier

--- function_modifier.sol
+++ function_modifier.sol
@@ -1,3 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
+
+
+contract MyModifier {
+    
+    uint256 public myNum = 0;
+    bool public paused = false;
+    address public owner;
+    
+    constructor () {
+        owner = address(msg.sender);
+    }
+    
+    modifier isNotPaused(bool _bypass) {
+        if(_bypass == true) {
+            require(paused == false, "Contract is paused");
+        }
+        _;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+    
+    function setPaused() public {
+        paused = true;
+    }
+    
+    function addToNum() public isNotPaused(true) onlyOwner {
+        myNum++;
+    }
+   
+     function sunFromNum() public isNotPaused(false) onlyOwner {
+        myNum--;
+    }
+
+}
